cc157328f5862fa54255289dc429fb2b6e6a6b82
Mohamad Reza Khatami <si2ed@live.com>
2018-07-14 16:42:26 +0430

    Some

--- Goldoon.WindowsService/Properties/AssemblyInfo.cs
+++ Goldoon.WindowsService/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Goldoon.WindowsService")]
+[assembly: AssemblyTitle("Telegram Shopping Bot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Me")]
 [assembly: AssemblyProduct("Goldoon.WindowsService")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
